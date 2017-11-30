57d94e7b7a960d16aa2978903b339ebc4ff84bab
Андрей Носов <andreinosov@MacBook-Air-Andrej.local>
2017-11-30 08:23:56 +0500

    First try

--- async.js
+++ async.js
@@ -8,6 +8,45 @@ exports.runParallel = runParallel;
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
  */
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    let result = []; // массив с результатами
+    let jobCounter = 0; // текущий элемент jobs
+    let end = 0; // условие конца
+
+    return new Promise((resolve) => {
+        // проверяю, если jobs пуст, тогда возвращаю []
+        if (jobs.length === 0) {
+            resolve(result);
+        }
+        // создаю promise на каждый язык в jobs
+        let promisesWithTimeout = jobs.map((job) => () =>
+            new Promise((_resolve, reject) => {
+                job().then(_resolve, reject);
+                // если выремя дошло до timeout, тогда reject с ошибкой
+                setTimeout(() => reject(new Error('PROMISE TIMEOUT!!!')), timeout);
+            })
+        );
+        // для каждого эл. в jobs вызывается обработчик
+        function translator(job) {
+            let i = jobCounter++;
+            let handler = function (data) {
+                checker(data, i);
+            };
+            job().then(handler);
+        }
+        // проверяет, когда можно закончить перевод
+        function checker(data, index) {
+            result[index] = data;
+            end = end + 1;
+            if (jobs.length === end) {
+                resolve(result);
+            }
+            if (jobCounter < jobs.length) {
+                translator(jobs[jobCounter]);
+            }
+        }
+        // ограничение по колличеству промисов
+        promisesWithTimeout.slice(0, parallelNum).forEach(job => translator(job));
+    });
 }
